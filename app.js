344cd4ed96d7201b3676b7af6fdc2070ec5ffdcd
maroneed <gabriell.appella@gmail.com>
2020-06-29 18:28:51 -0300

    Subida del Proyecto

---
+++ app.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const app = express();
+
+
+
+const path = require('path');
+const port = 3002
+const router = express.Router();
+app.use(express.static(('public')));
+
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+//add the router
+app.use('/', router);
+// static files
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
