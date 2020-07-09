bf8de6179874c140ae04f9b3cc0e9fea68b34213
Ayelen Palmieri <ayelenpalmieri9012@gmail.com>
2020-07-09 00:25:38 -0300

    modificacion de footer header y cards en cada uno de los tramites y turnos

--- app.js
+++ app.js
@@ -26,6 +26,22 @@ router.get('/pages/turnos/dni/dni.html', function(req,res){
   res.sendFile(path.join(__dirname+'/pages/turnos/dni/dni.html'));
 });
 
+router.get('/pages/turnos/defuncion/defuncion.html', function(req,res){
+  res.sendFile(path.join(__dirname+'/pages/turnos/defuncion/defuncion.html'));
+});
+
+router.get('/pages/turnos/nacimiento/nacimiento.html', function(req,res){
+  res.sendFile(path.join(__dirname+'/pages/turnos/nacimiento/nacimiento.html'));
+});
+
+router.get('/pages/turnos/matrimonio/matrimonio.html', function(req,res){
+  res.sendFile(path.join(__dirname+'/pages/turnos/matrimonio/matrimonio.html'));
+});
+
+router.get('/pages/turnos/divorcio/divorcio.html', function(req,res){
+  res.sendFile(path.join(__dirname+'/pages/turnos/divorcio/divorcio.html'));
+});
+
 //add the router
 app.use('/', router);
 // static files
